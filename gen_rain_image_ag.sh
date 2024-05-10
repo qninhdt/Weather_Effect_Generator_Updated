e1de31a098254af642fdbbb3e8a1eca79e4d7825
qninhdt <qndt123@gmail.com>
2024-05-11 04:55:32 +0700

    abc

---
+++ gen_rain_image_ag.sh
@@ -0,0 +1,4 @@
+python Rain_Effect_Generator.py \
+    --clear_path ./data/gan/rain_images \
+    --depth_path ./data/depth_maps \
+    --save_folder ./data/analytical_gan/rain_images
---
+++ gen_snow_image_ag.sh
@@ -0,0 +1,4 @@
+python  Snow_Effect_Generator.py \
+    --clear_path ./data/gan/snow_images \
+    --depth_path ./data/depth_maps \
+    --save_folder ./data/analytical_gan/snow_images
