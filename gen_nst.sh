e07c546d66f24c0eb3b94a1ec41e9291fee22055
qninhdt <qndt123@gmail.com>
2024-05-11 17:47:15 +0700

    abc

--- gen_nst.sh
+++ gen_nst.sh
@@ -1,6 +1,8 @@
 python Neural_Style_Transfer.py \
-    --content-imgs ./content_images/ \
-    --style-imgs ./style_images/ \
-    --save-folder ./save_images/ \
+    --content-imgs ./data/images/ \
+    --style-imgs ./data/styles/ \
+    --save-folder ./data/nst/rain_images \
     --vgg ./checkpoints/rain_vgg_512 \
+    --min-step 100 \
+    --max-step 200 \
     --cuda
